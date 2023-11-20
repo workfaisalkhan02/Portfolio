f07c57606a2334b94d2f6113de1597c5abed7954
Faisal Khan <workfaisalkhan02@gmail.com>
2023-11-20 14:46:13 +0530

    Hello World!

---
+++ src/Components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Home() {
+    return (
+      <div className="App">
+      <h1>app is running</h1>
+      <h1>app is running</h1>
+      </div>
+    );
+  }
+  export default Home;
